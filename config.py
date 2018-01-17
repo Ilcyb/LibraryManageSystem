735e4f365ec0b46ec6c161f9f6dbdb84a4113889
ilcyb <hybmail1996@gmail.com>
2018-01-17 21:49:39 +0800

    将数据库连接所采用的字符集更改为utf-8

--- config.py
+++ config.py
@@ -9,18 +9,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + environ.get('local_mysql_username') + \
-                              ':' + environ.get('mysql_password') + '@localhost/library_dev'
+                              ':' + environ.get('mysql_password') + '@localhost/library_dev?charset=utf8'
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + environ.get('local_mysql_username') + \
-                              ':' + environ.get('mysql_password') + '@localhost/library_test'
+                              ':' + environ.get('mysql_password') + '@localhost/library_test?charset=utf8'
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + environ.get('local_mysql_username') + \
-                              ':' + environ.get('mysql_password') + '@localhost/library'
+                              ':' + environ.get('mysql_password') + '@localhost/library?charset=utf8'
 
 
 configs = {
