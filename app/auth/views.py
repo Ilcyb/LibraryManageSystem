e53183589a1e962b7770306184c134574f9ecade
ilcyb <hybmail1996@gmail.com>
2018-01-17 21:53:08 +0800

    将登陆和注册api接口所返回的是否成功的属性由0和1改为true和false

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from .. import db
 from ..models import User
 from flask import request, g, session, jsonify, url_for
 
+
 @auth.route('/login', methods=['POST'])
 def login_api():
     """
@@ -11,8 +12,8 @@ def login_api():
     :method post
     :param username 登陆框中的用户名
     :param password 登陆框中的密码
-    :return json {'login_statu':1,'page':'url'} 登陆成功  url为登陆成功后要跳转的页面url
-    :return json {'login_statu':0,'error':'reason'} 登陆失败 reason为错误信息
+    :return json {'login_statu':true,'page':'url'} 登陆成功  url为登陆成功后要跳转的页面url
+    :return json {'login_statu':false,'error':'reason'} 登陆失败 reason为错误信息
     """
     username_or_email = request.form['username']
     password = request.form['password']
@@ -21,9 +22,10 @@ def login_api():
         session['username'] = user.username
         session['id'] = user.user_id
         session['login'] = True
-        return jsonify({'login_statu':1, 'page':request.referrer or url_for('main.index')}), 200
+        return jsonify({'login_statu': True, 'page': request.referrer or url_for('main.index')}), 200
     else:
-        return jsonify({'login_statu':0,'error':'用户名或密码错误'}), 401
+        return jsonify({'login_statu': False, 'error': '用户名或密码错误'}), 401
+
 
 @auth.route('/register', methods=['POST'])
 def register_api():
@@ -34,22 +36,21 @@ def register_api():
     :param username
     :param password
     :param email
-    :return json {'register_statu':1,'page':'url'} 注册成功 url为注册成功后要跳转的页面url
-    :return json {'register_statu':0,'error':'reason'} 注册失败 reason为错误信息
+    :return json {'register_statu':true,'page':'url'} 注册成功 url为注册成功后要跳转的页面url
+    :return json {'register_statu':false,'error':'reason'} 注册失败 reason为错误信息
     """
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
     if User.query.filter(User.username == username).count() != 0:
-        return jsonify({'register_statu':0, 'error':'此用户名已被注册'}), 200
+        return jsonify({'register_statu': False, 'error': '此用户名已被注册'}), 200
     elif User.query.filter(User.email == email).count() != 0:
-        return jsonify({'register_statu':0, 'error':'此电子邮箱已被注册'}), 200
+        return jsonify({'register_statu': False, 'error': '此电子邮箱已被注册'}), 200
     else:
         new_user = User(username, password, email)
         db.session.add(new_user)
         db.session.commit()
-        user = User.query.filter(User.username == username).first()
-        session['username'] = user.username
-        session['id'] = user.user_id
+        session['username'] = new_user.username
+        session['id'] = new_user.user_id
         session['login'] = True
-        return jsonify({'register_statu':1, 'page':request.referrer or url_for('main.index')}), 200
+        return jsonify({'register_statu': True, 'page': request.referrer or url_for('main.index')}), 200
