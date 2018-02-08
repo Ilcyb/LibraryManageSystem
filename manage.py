1b3a02767c11fb3416a6393d24cc0fce870d0761
ilcyb <hybmail1996@gmail.com>
2018-02-08 14:50:04 +0800

    migrate = Migrate(app) -> migrate = Migrate(app, db)

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('default')
 manager = Manager(app)
-migrate = Migrate(app)
+migrate = Migrate(app, db)
 
 manager.add_command('db', MigrateCommand)
 
