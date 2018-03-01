b52bbe8c66c7871418a04223ebab6176dd1e3325
ilcyb <hybmail1996@gmail.com>
2018-03-01 10:14:33 +0800

    添加首页，注册，登陆等路由

--- app/main/views.py
+++ app/main/views.py
@@ -4,4 +4,12 @@ from flask import render_template
 
 @main.route('/')
 def index():
-    return 'test'
+    return render_template('index.html')
+
+@main.route('/login')
+def login():
+    return render_template('login.html')
+
+@main.route('/register')
+def register():
+    return render_template('register.html')
