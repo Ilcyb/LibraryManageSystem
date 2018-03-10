c33d1d2457aa088cf239a1e24167c1b8a92946b8
ilcyb <hybmail1996@gmail.com>
2018-03-10 23:01:11 +0800

    实现了登录要求和管理员要求装饰器

--- app/utils/decorators.py
+++ app/utils/decorators.py
@@ -6,3 +6,10 @@ def login_required(func):
             return func(*args, **kwargs)
         else:
             return url_for('main.login')
+
+def admin_required(func):
+    def handle_args(*args, **kwargs):
+        if session.get('isAdmin', None) == True:
+            return func(*args, **kwargs)
+        else:
+            return url_for('admin.admin_login')
